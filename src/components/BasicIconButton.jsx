59598e2346085d6931e18fe7e5db4b3af6509486
jonathanfoxhp <jonathan.fox@hp.com>
2025-02-18 16:20:49 -0500

    fix header button color

--- src/components/BasicIconButton.jsx
+++ src/components/BasicIconButton.jsx
@@ -1,6 +1,9 @@
 function BasicIconButton({ icon, ...other }) {
   return (
-    <button className="icon-button--basic" {...other}>
+    <button
+      className="icon-button--basic !w-10 !h-10 !bg-[#828398/20]"
+      {...other}
+    >
       {icon}
     </button>
   );
